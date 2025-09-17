fe2378c1b3415b663cbcbd279fc8180da66e7941
Pedro Madruga <pedro.madruga@liferay.com>
2025-09-17 18:33:03 -0300

    Added java file

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Test with");
+
+        List<String> list = new ArrayList<>();
+
+        list.add("HELLO");
+        list.add("WORLD");
+    }
+}
